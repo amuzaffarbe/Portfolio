08d54e0e17d30b090ec8d2f7450b1a0cf1b223e0
amuzaffarbe <amuzaffarbe@gmail.com>
2022-04-01 17:10:43 +0500

    Color changed

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -1,7 +1,6 @@
 let color = document.querySelector(".color")
 let blackColor = document.querySelector(".black-color")
 let whiteColor = document.querySelector(".white-color")
-let redColor = document.querySelector(".red-color")
 
 // localStorage.removeItem("color")
 color.classList.add(localStorage.getItem("color"))
@@ -18,13 +17,7 @@ whiteColor.addEventListener("click", function () {
     color.classList.add(localStorage.getItem("color"))
 })
 
-redColor.addEventListener("click", function () {
-    color.classList.remove(localStorage.getItem("color"))
-    localStorage.setItem("color", "red")
-    color.classList.add(localStorage.getItem("color"))
-})
-
-// Contact page
+    // Contact page
 
 let names = document.querySelector(".ism-c")
 let telefon = document.querySelector(".telefon-c")
@@ -32,12 +25,20 @@ let savol = document.querySelector(".savol-c")
 let telegram = document.querySelector(".telegram-c")
 let sendBtn = document.querySelector(".contact-send")
 
+const sender = document.querySelector("#sender"),
+    toast = document.querySelector(".toast"),
+    closeIcon = document.querySelector(".close"),
+    progress = document.querySelector(".progress"),
+    text1 = document.querySelector(".text1"),
+    text2 = document.querySelector(".text2");
+
+let timer1, timer2;
+
 
 sendBtn.addEventListener("click", () => {
     if (names.value == "" && telefon.value == "" && savol.value == "" && telegram.value == "") {
         return;
     } else {
-       
         var my_text = `Yangi Feedback :%0A%0AIsmi - ${names.value}, %0A%0ASizga yuborgan matni - %0A${savol.value}, %0A%0ATelefon raqami - ${telefon.value}, %0A%0ATelegrami - ${telegram.value}`
 
         var token = "5252628125:AAFtsmD2VCHGGcq-J-7UB9sZ12q18LMm4U0";
@@ -47,7 +48,33 @@ sendBtn.addEventListener("click", () => {
         let api = new XMLHttpRequest();
         api.open("GET", url, true);
         api.send();
-        alert("Malumotlar Muzaffarbekga yuborildi ✅")
-        window.location.reload()
+        // alert("Malumotlar Muzaffarbekga yuborildi ✅")
+
+        toast.classList.add("active");
+        progress.classList.add("active");
+
+        timer1 = setTimeout(() => {
+            toast.classList.remove("active");
+        }, 5000); //1s = 1000 milliseconds
+
+        timer2 = setTimeout(() => {
+            progress.classList.remove("active");
+        }, 5300);
+
+        setTimeout(() => {
+            window.location.reload()
+        }, 5000);
     }
 })
+
+closeIcon.addEventListener("click", () => {
+    toast.classList.remove("active");
+
+    setTimeout(() => {
+        progress.classList.remove("active");
+    }, 300);
+
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    window.location.reload()
+});
